fabf4fe1fdf6310b7b24f7f40963f7e899aa8aec
ecstasybtw <ecstasybtwru@gmail.com>
2026-03-03 02:06:18 +0500

    chore: misc update _request function

--- github_analytics/core/services.py
+++ github_analytics/core/services.py
@@ -1,6 +1,9 @@
 import time
 from datetime import datetime, timezone
+
+import allauth.socialaccount.models
 import requests
+from typing import Any
 try:
     from allauth.socialaccount.models import SocialToken
 except Exception:
@@ -13,6 +16,9 @@ class GitHubNoTokenException(Exception):
 
 class GitHubUserException(Exception):
     """Cannot get an authenticated user."""
+
+class GitHubJSONException(Exception):
+    """Some problems with JSON response from API"""
 
 def _get_user_token(user):
     if SocialToken is None or not user.is_authenticated:
@@ -28,22 +34,26 @@ def _get_user_token(user):
     except SocialToken.DoesNotExist:
         return None
 
-def _get_headers(token):
+def _get_headers(token) -> dict[str, str]:
     return {
         "Accept": "application/vnd.github+json",
         "Authorization": f"Bearer {token}",
         "X-GitHub-Api-Version": "2022-11-28",
     }
 
-def _request(user, endpoint: str, method:str):
+def _request(user, endpoint: str, method: str) -> dict[str, Any] | None:
     token = _get_user_token(user)
     if token:
         headers = _get_headers(token)
         try:
-            response = requests.request(url=f'{URL + endpoint}', method=method, headers=headers)
-            payload = response.json()
-            return payload
-        except Exception:
+            response = requests.request(url=f'{URL + endpoint}', method=method, headers=headers, timeout=60)
+            if response.status_code == 200:
+                payload = response.json()
+                return payload
+            else:
+                raise GitHubJSONException
+        except (Exception, requests.exceptions.Timeout):
             return None
     else:
-        raise GitHubUserException
+        raise GitHubNoTokenException
+
