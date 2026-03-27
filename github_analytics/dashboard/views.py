74fec6a41e70e59da8d24d6f7791cecbaee5e224
ecstasybtw <ecstasybtwru@gmail.com>
2026-03-27 20:37:48 +0500

    feat: new analytics block on repo_detail page

--- github_analytics/dashboard/views.py
+++ github_analytics/dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 import integrations.models as integration_models
 from integrations.services import _get_metrics
 from django.views.decorators.http import require_GET
+from analytics.services import get_basic_metrics
 
 
 @require_GET
@@ -43,8 +44,11 @@ def profile_view(request):
 def repo_detail_view(request, pk):
     repo = integration_models.GitHubRepo.objects.get(id=pk)
 
+    basic_metrics = get_basic_metrics(repo)
+
     context = {
-        'repo': repo
+        'repo': repo,
+        'basic_metrics': basic_metrics
     }
 
     return render(request, 'dashboard/repo_detail.html', context=context)
